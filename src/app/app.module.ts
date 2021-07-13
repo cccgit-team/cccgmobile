514f443073acf3139221a69a0f3187fc1fcc6d7b
Prosper Nsengiyumva <n.prosper@gmail.com>
2021-07-13 15:20:22 -0600

    Events and Push Notifications
    Events and Push Notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,17 +13,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+  ],
   providers: [
     StreamingMedia,
-    YoutubeVideoPlayer, 
+    YoutubeVideoPlayer,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     YtService,
     YoutubeVideoPlayer,
-    InAppBrowser
+    InAppBrowser,
   ],
   bootstrap: [AppComponent],
 })
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { EventsService } from '../services/events.service';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
+  eventCount: any;
+  events: Event[];
+  constructor(private eventsService: EventsService) {}
 
-  constructor() {}
-
+  ngOnInit() {
+    this.eventsService.getEvents().subscribe((res) => {
+      this.eventCount = res.length > 0 ? res.length : '';
+    });
+  }
 }
